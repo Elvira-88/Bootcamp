8c339b2f22f7feb0bb5d89c87cefb699f92a8177
elvira <elvipt88@gmail.com>
2021-04-20 14:01:47 +0200

    Ejercicio 1 y 2 React

--- React/cheatsheet/my-app/src/components/FirstComponent.js
+++ React/cheatsheet/my-app/src/components/FirstComponent.js
@@ -1,12 +1,39 @@
-export default function FirstComponent() {
+import {Fragment} from "react";
+
+export default function FirstComponent(props) {
+
+    console.log(props, typeof props);
+
+    console.log(props.title);
+
+    return (
+        <Fragment>
+            <h3>{props.title}</h3>
+            <small>{props.date ? props.date : "No hay fecha"}</small>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, omnis!</p>
+            <button>Click me!</button>
+        </Fragment>
+    )
+}
+
+// export default FirstComponent (Alternativa)
+// SOLO se puede un export default por archivo
+
+function SecondComponent() {
+    return (
+        <>
+            <h1>Soy otro componente</h1>
+        </>
+    )
+}
+
+function ThirdComponent() {
     return (
         <div>
-            <h3>Título</h3>
-            <small>16 de Agosto de 2025</small>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, omnis!</p>
+            <h1>Soy otro componente más</h1>
         </div>
     )
 }
 
-//
+export {SecondComponent, ThirdComponent};
 

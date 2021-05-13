8aced6e1ae20f0a8c78330e3b520bc19510e61f1
Elvira-88 <elvipt88@gmail.com>
2021-05-13 08:45:30 +0200

    No de ej1

--- Node/ejercicio1/rest_server.js
+++ Node/ejercicio1/rest_server.js
@@ -3,4 +3,36 @@ const app = express();
 
 app.use(express.json());
 
+
+app.get("/", (req, res) => {
+    res.json({message: "Apartado 3"});
+});
+
+app.put("/:id", (req, res) => {
+    let id = req.params.id;
+    res.json({message: `Apartado 4: ${id}`});
+});
+
+app.delete("/", (req, res) => {
+    res.json({message: "Apartado 5"});
+});
+
+app.post("/", (req, res) => {
+    let body = req.body;
+
+    if(body.user) {
+        res.status(200).json(
+            {
+                message: `Apartado 6: ${body.user}`, 
+            })
+    } else {
+        res.status(400).json(
+            {
+                message: "El nombre es requerido", 
+            })
+    }
+  ;
+});
+
 app.listen(3000);
+
